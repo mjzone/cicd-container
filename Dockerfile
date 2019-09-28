1bfa543abe1b7b8822ccf758dcbcca970feff729
Manoj Fernando <manojf@99x.lk>
2019-09-28 14:54:01 +0530

    Added health check endpoint

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,6 @@ COPY package.json /app
 RUN npm install
 
 COPY . /app
+
+EXPOSE 80
 CMD ["npm", "start"]
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.get("/health", (req, res) => {
+  res.status(200);
+  res.send("healthy");
+});
 
 app.listen(3000, () => {
-	console.log('App listening on port 3000!');
+  console.log("App listening on port 3000!");
 });
