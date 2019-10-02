d297854346bfc88e73af7171439a7b5d674c94ed
Manoj Fernando <manojf@99x.lk>
2019-10-03 04:10:44 +0530

    Changed display text

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello Youtube!"));
+app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/health", (req, res) => {
   res.status(200);
